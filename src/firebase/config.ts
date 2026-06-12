ac951fff1409bb0dfa8f7dcd91c079e0f9e543ce
Frehiwot702 <frehiwot.tewodros112@gmail.com>
2026-06-12 16:48:07 +0300

    fix/ update dependencies and configure Firebase environment variables

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,14 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9wexCHkmtv-G8tsKyUVaFbO-vtuaFff0",
-  authDomain: "live-tracker-6b2d8.firebaseapp.com",
-  projectId: "live-tracker-6b2d8",
-  storageBucket: "live-tracker-6b2d8.firebasestorage.app",
-  messagingSenderId: "491275493669",
-  appId: "1:491275493669:web:63f396a3e5719307b0facc"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGR_BUCKUET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
-
 
 const app = initializeApp(firebaseConfig);
 
